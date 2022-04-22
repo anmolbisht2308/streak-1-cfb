1df38324db299d6c35ae5cb0ce69a8d1049c7562
Anmol Bisht <85604559+anmolbisht2308@users.noreply.github.com>
2022-04-22 20:30:07 +0530

    Add files via upload
    day 22

---
+++ pattern4 c++.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+# include<iostream>
+using namespace std;
+    
+
+
+
+int main()
+{
+	int count=1;
+	for(int i=0;i<4+1;i++)
+	{
+	     	for(int j=0;j<4;j++)
+		{
+		if(j>4-i-1)
+		cout<<"*";
+		else
+		cout<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
